f41bc0c17e1548dd452c06645b35546664362683
avrzll <avrizalrendiprayoga@gmail.com>
2025-06-19 12:22:31 +0700

    c

--- src/models/wisata.models.js
+++ src/models/wisata.models.js
@@ -5,38 +5,38 @@ export class Wisata {
 
   async createWisata(nama, deskripsi, kota_id) {
     const query =
-      "INSERT INTO wisata (nama, deskripsi, kota_id) VALUES (?, ?, ?)";
+      "INSERT INTO tempat_wisata (nama, deskripsi, kota_id) VALUES (?, ?, ?)";
     let result = await this.db.query(query, [nama, deskripsi, kota_id]);
     return result;
   }
 
   async getWisataById(id) {
-    const query = "SELECT * FROM wisata WHERE id = ?";
+    const query = "SELECT * FROM tempat_wisata WHERE id = ?";
     let result = await this.db.query(query, [id]);
     return result;
   }
 
   async getWisata() {
-    const query = "SELECT * FROM wisata";
+    const query = "SELECT * FROM tempat_wisata";
     let result = await this.db.query(query);
     return result;
   }
 
   async getWisataByKotaId(id) {
-    const query = "SELECT * FROM wisata WHERE kota_id = ?";
+    const query = "SELECT * FROM tempat_wisata WHERE kota_id = ?";
     let result = await this.db.query(query, [id]);
     return result;
   }
 
   async updateWisata(id, nama, deskripsi, kota_id) {
     const query =
-      "UPDATE wisata SET nama = ?, deskripsi = ?, kota_id = ? WHERE id = ?";
+      "UPDATE tempat_wisata SET nama = ?, deskripsi = ?, kota_id = ? WHERE id = ?";
     let result = await this.db.query(query, [nama, deskripsi, kota_id, id]);
     return result;
   }
 
   async deleteWisata(id) {
-    const query = "DELETE FROM wisata WHERE id = ?";
+    const query = "DELETE FROM tempat_wisata WHERE id = ?";
     let result = await this.db.query(query, [id]);
     return result;
   }
